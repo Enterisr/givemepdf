4e4106310dc173ed33ff088fb926d1f1897024a0
drunk <drunktolstoy@gmail.com>
2024-12-30 10:58:50 +0200

    fixing selector

--- content.js
+++ content.js
@@ -3,33 +3,40 @@ console.log("letmepdf is running on this page!");
 (function () {
     "use strict";
     window.onload = () => {
-        document.querySelectorAll(".activityname .aalink").forEach((elm) => {
-            elm.addEventListener("click", (eve) => {
-                if (!eve.target.getAttribute("data-letmepdf")) {
-                    eve.preventDefault();
+        document
+            .querySelectorAll(
+                ".activityname .aalink[href*='/mod/resource/view.php']"
+            )
+            .forEach((elm) => {
+                elm.addEventListener("click", (eve) => {
+                    if (!eve.target.getAttribute("data-letmepdf")) {
+                        eve.preventDefault();
 
-                    fetch(elm.href)
-                        .then((d) => {
-                            return d.text();
-                        })
-                        .then((text) => {
-                            const regex =
-                                /object id="resourceobject"\s+data="([^"]+)"/;
-                            const match = text.match(regex);
-                            const dataUrl = match ? match[1] : null;
+                        fetch(elm.href)
+                            .then((d) => {
+                                return d.text();
+                            })
+                            .then((text) => {
+                                const regex =
+                                    /object id="resourceobject"\s+data="([^"]+)"/;
+                                const match = text.match(regex);
+                                const dataUrl = match ? match[1] : null;
 
-                            if (dataUrl) {
-                                console.log("changeing link...");
-                                elm.href = dataUrl;
-                                elm.setAttribute("data-letmepdf", "true");
-                                elm.setAttribute("target", "_blank");
-                                elm.setAttribute("rel", "noopener noreferrer");
-                                eve.target.click();
-                            }
-                        });
-                }
+                                if (dataUrl) {
+                                    console.log("changeing link...");
+                                    elm.href = dataUrl;
+                                    elm.setAttribute("data-letmepdf", "true");
+                                    elm.setAttribute("target", "_blank");
+                                    elm.setAttribute(
+                                        "rel",
+                                        "noopener noreferrer"
+                                    );
+                                    eve.target.click();
+                                }
+                            });
+                    }
+                });
             });
-        });
     };
 })();
 /*if (dataUrl) {
